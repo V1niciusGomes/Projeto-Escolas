5e78676c6161f5a6f40b267e7374b45072cdbac6
VINÍCIUS GOMES SENS <vinicius_sens@estudante.sesisenai.org.br>
2026-08-12 09:45:13 -0300

    commit

--- src/main/java/com/codemasters/repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codemasters.repository;
-
-import com.codemasters.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
